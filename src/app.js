7de92b82ea85ceb695136942178c0da301edd06f
pengchihui1 <pengchihui1@gmail.com>
2022-10-22 16:04:26 +0800

    多个marker多个影片

--- src/app.js
+++ src/app.js
@@ -5,71 +5,78 @@ $("#start-btn").on("click", () => {
   $("#start-modal").hide();
 });
 
-// const exampleTarget = document.querySelector('#target');
-// // detect target found
-// exampleTarget.addEventListener("targetFound", event => {
-//   var videoEl = exampleTarget.getAttribute("material").src;
-//   if (!videoEl) {
-//     return;
-//   }
-//   exampleTarget.object3D.visible = true;
-//   videoEl.load()
-//   videoEl.muted = false
-//   videoEl.play();
-// });
+const exampleTarget1 = document.querySelector('#target1');
+// detect target found
+exampleTarget1.addEventListener("targetFound", event => {
+  var videoEl = exampleTarget1.getAttribute("material").src;
+  if (!videoEl) {
+    return;
+  }
+  exampleTarget1.object3D.visible = true;
+  videoEl.currentTime = 0//重頭播放
+  videoEl.muted = false
+  videoEl.play();
+});
 // // detect target lost
-// exampleTarget.addEventListener("targetLost", event => {
-//   var videoEl = exampleTarget.getAttribute("material").src;
-//   this.el.object3D.visible = false;
-//   videoEl.muted = true
-// });
-
-AFRAME.registerComponent("play-on-click", {
-  init: function () {
-    this.onClick = this.onClick.bind(this);
-  },
-  play: function () {
-    window.addEventListener("click", this.onClick);
-  },
-  pause: function () {
-    window.removeEventListener("click", this.onClick);
-  },
-  onClick: function (evt) {
-    var videoEl = this.el.getAttribute("material").src;
-    if (!videoEl) {
-      return;
-    }
-    this.el.object3D.visible = true;
-    videoEl.unload();//销毁已在播放的
-    videoEl.muted = false//靜音轉為有聲音
-    videoEl.play();//播放此影片
-  }
+exampleTarget1.addEventListener("targetLost", event => {
+  var videoEl = exampleTarget1.getAttribute("material").src;
+  videoEl.pause()
 });
 
-/* global AFRAME */
-AFRAME.registerComponent("hide-on-play", {
-  schema: { type: "selector" },
-  init: function () {
-    this.onPlaying = this.onPlaying.bind(this);
-    this.onPause = this.onPause.bind(this);
-    this.el.object3D.visible = !this.data.playing;
-  },
-  play: function () {
-    if (this.data) {
-      this.data.addEventListener("playing", this.onPlaying);
-      this.data.addEventListener("pause", this.onPause);
-    }
-  },
-  pause: function () {
-    if (this.data) {
-      this.data.removeEventListener("playing", this.onPlaying);
-      this.data.removeEventListener("pause", this.onPause);
-    }
-  },
-  onPlaying: function (evt) {
-    this.el.object3D.visible = false;
-  },
-  onPause: function (evt) {
-    this.el.object3D.visible = true;
+const exampleTarget2 = document.querySelector('#target2');
+var videoEl = exampleTarget2.getAttribute("material").src;
+// detect target found
+exampleTarget2.addEventListener("targetFound", event => {
+  var videoEl = exampleTarget2.getAttribute("material").src;
+  if (!videoEl) {
+    return;
   }
+  exampleTarget2.object3D.visible = true;
+  videoEl.currentTime = 0//重頭播放
+  videoEl.muted = false
+  videoEl.play();
 });
+// // detect target lost
+exampleTarget2.addEventListener("targetLost", event => {
+  var videoEl = exampleTarget2.getAttribute("material").src;
+  videoEl.pause()
+});
+
+const exampleTarget3 = document.querySelector('#target3');
+var videoEl = exampleTarget3.getAttribute("material").src;
+// detect target found
+exampleTarget3.addEventListener("targetFound", event => {
+  var videoEl = exampleTarget3.getAttribute("material").src;
+  if (!videoEl) {
+    return;
+  }
+  exampleTarget3.object3D.visible = true;
+  videoEl.currentTime = 0//重頭播放
+  videoEl.muted = false
+  videoEl.play();
+});
+// // detect target lost
+exampleTarget3.addEventListener("targetLost", event => {
+  var videoEl = exampleTarget3.getAttribute("material").src;
+  videoEl.pause()
+});
+
+const exampleTarget4 = document.querySelector('#target4');
+var videoEl = exampleTarget4.getAttribute("material").src;
+// detect target found
+exampleTarget4.addEventListener("targetFound", event => {
+  var videoEl = exampleTarget4.getAttribute("material").src;
+  if (!videoEl) {
+    return;
+  }
+  exampleTarget4.object3D.visible = true;
+  videoEl.currentTime = 0//重頭播放
+  videoEl.muted = false
+  videoEl.play();
+});
+// // detect target lost
+exampleTarget4.addEventListener("targetLost", event => {
+  var videoEl = exampleTarget4.getAttribute("material").src;
+  videoEl.pause()
+});
+
